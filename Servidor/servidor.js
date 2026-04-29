53de834fea6efc66182bdacee615440bd77a9b51
Samuel__ <samuelmtonon@gmail.com>
2026-04-28 22:13:07 -0300

    Add files via upload
    VERSAO DA AULA 10

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,13 +1,53 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-// libera a pasta public
-app.use(express.static('public'));
+// // libera a pasta public
+// app.use(express.static('public'));
 
-// porta 80 (OBRIGATÓRIO)
-const PORT = 80;
+// // porta 80 (OBRIGATÓRIO)
+// const PORT = 80;
 
-// inicia servidor
-app.listen(PORT, '0.0.0.0', () => {
-    console.log('Servidor rodando na porta 80');
-});
+// // inicia servidor
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log('Servidor rodando na porta 80');
+// });
+
+var http = require('http');
+var express = require('express');
+var colors = require('colors');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
+
+var server = http.createServer(app);
+server.listen(80);
+
+console.log('Servidor rodando ...'.rainbow);
+
+app.get('/', function (requisicao, resposta){
+resposta.redirect('home.html')
+})
+
+app.get('/inicio', function (requisicao, resposta){
+var nome = requisicao.query.info;
+console.log(nome);
+})
+
+app.post('/inicio', function (requisicao, resposta){
+var data = requisicao.body.data;
+console.log(data);
+})
+
+app.get('/cadastro',function (requisicao, resposta){
+var nome = requisicao.query.nome;
+var sobrenome = requisicao.query.sobrenome;
+var nascimento = requisicao.query.nascimento;
+var civil = requisicao.query.civil;
+
+resposta.render('resposta_cadastro', {nome, sobrenome, nascimento, civil})
+})
